14ec1ef01287f09ae469332302cded7eefd54a19
joaomacedocb <joaomacedocb@gmail.com>
2024-12-31 08:05:55 -0300

    Create songs model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Song(models.Model):
+    id_song = models.UUIDField(primary_key=True, default= uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    duration = models.DurationField()
+    record = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
