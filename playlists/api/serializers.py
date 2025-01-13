d27ce146c1a7913556daab6d2a9ab95d1965a495
joaomacedocb <joaomacedocb@gmail.com>
2025-01-12 21:10:29 -0300

    Ajust playlist serializer

--- playlists/api/serializers.py
+++ playlists/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 
 from playlists.models import Playlist
+from songs.models import Song
+
+
+class SimplifiedSongsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id_song','title']
 
 class PlaylistsSerializers(serializers.ModelSerializer):
+    songs = SimplifiedSongsSerializer(many= True, read_only=True)
+    
     class Meta:
         model = Playlist
-        fields = "__all__"
+        fields = ['id', 'name', 'description', 'songs', 'owner', 'created_at', 'updated_at']
